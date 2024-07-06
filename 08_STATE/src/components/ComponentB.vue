5089b8b8cceed0b15784f0bb4b637e57e708a21d
lucia11 <1263898254@qq.com>
2024-07-06 19:03:25 +0800

    同步更改

--- 08_STATE/src/components/ComponentB.vue
+++ 08_STATE/src/components/ComponentB.vue
@@ -5,9 +5,10 @@ const isShow = ref(true);
 </script>
 
 <template>
-    <h3>ComponentB</h3>
-    <ul>
-        <li v-if="isShow"><ComponentC></ComponentC></li>
-    </ul>
-    
+    <div>
+        <h3>ComponentB</h3>
+        <ul>
+            <li v-if="isShow"><ComponentC></ComponentC></li>
+        </ul>
+    </div>
 </template>
