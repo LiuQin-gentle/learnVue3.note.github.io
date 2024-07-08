00a2abfd73034a483a4c24996e88c287fdc8af2e
lucia11 <1263898254@qq.com>
2024-07-08 20:23:47 +0800

    完成清空购物车

--- 09_meals/src/components/Cart/CartDetails.vue
+++ 09_meals/src/components/Cart/CartDetails.vue
@@ -1,16 +1,41 @@
 <script setup>
 import Mask from "../UI/Mask.vue";
+import Dialog from "../UI/Dialog.vue"
 import MealList from "../Meals/MealList.vue";
 import { useMealsStore } from "@/store/meals";
+import { ref } from "vue";
 const meals = useMealsStore();
+const showDialog = ref(false);
+const msg = "确认清空购物车吗?"
+
+const emits = defineEmits();
+const okHandler = () => {
+    /*
+        ① 把购物车清空 
+        ② 关闭对话框 Dialog
+        ③ 关闭CartDetails 
+    */
+    meals.clearCart();
+    // showDialog.value = false;
+    emits('hide');
+}
 </script>
 
 <template>
-  <Mask>
+    
+  <Mask style="z-index: 999;">
+    <Dialog 
+        @ok = "okHandler"
+        :is-show = "showDialog"
+        @hide = "showDialog = false"
+        :msg = "msg"
+        ></Dialog>
     <div class="cart-details">
       <div class="header">
         <h2>餐品详情</h2>
-        <a href="javascript:;">
+        <a 
+            @click="showDialog = true" 
+            href="javascript:;">
             <i class="ri-delete-bin-line"></i>
             清空购物车
         </a>
---
+++ 09_meals/src/components/UI/Dialog.vue
@@ -0,0 +1,66 @@
+<script setup>
+    import Mask from './Mask.vue'
+    import {defineProps} from 'vue'; 
+    import {defineEmits} from 'vue';
+    const props = defineProps(["msg"]);
+    const emits = defineEmits(["ok", "hide"]);
+</script>
+
+<template>
+    <Mask>
+        <div class="dialog">
+            <p class="msg">{{ props.msg }}</p>
+            <div class="btn">
+                <button @click="$emit('hide')" class="cancel">取消</button>
+                <button 
+                    @click="() => {
+                        $emit('ok')
+                        $emit('hide')
+                    }" 
+                    class="ok">
+                    确认
+                </button>
+            </div>
+        </div>
+    </Mask>
+</template>
+
+<style scoped>
+.dialog{
+    position: absolute;
+    width: 700rem;
+    height: 400rem;
+    left: 0;
+    right: 0;
+    top: 250rem;
+    background-color: white;
+    border-radius: 40rem;
+    margin: 0 auto;
+}
+
+.msg{
+    height: 300rem;
+    font-size: 40rem;
+    text-align: center;
+    line-height: 260rem;
+}
+
+.btn{
+    display: flex;
+    justify-content: space-around;
+}
+
+button{
+    width: 300rem;
+    height: 80rem;
+    border: 1px solid #000;
+    background-color: white;
+    border-radius: 40rem;
+    font-size: 32rem;
+}
+
+.ok{
+    border: none;
+    background-color: rgb(248,188,0);
+}
+</style>
